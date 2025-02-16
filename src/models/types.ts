0ea7359f2c991663459abcc75ea09574d6cc7f51
fatekkl <mathtml.1105@gmail.com>
2025-02-16 16:19:26 -0300

    create file for save my interfaces & types

---
+++ src/models/types.ts
@@ -0,0 +1,37 @@
+import { Env } from "../../worker-configuration";
+
+type HttpMethod = "get" | "post" | "put" | "delete" | "patch";
+
+
+
+
+export type Route = {
+    method: string;
+    path: string;
+    handler: (request: Request, env: Env) => Promise<Response>;
+};
+
+export interface Post {
+    success: boolean;
+    post_id: number;
+    resource_id: string;
+    created_at: string;
+    // outros campos, se houver
+  }
+  
+
+
+export interface User {
+    id: string;
+    email: string;
+    utm_source: string;
+    utm_medium: string;
+    utm_campaign: string;
+    utm_channel: string;
+}
+
+export interface ApiResponse {
+    success: boolean,
+    data: Object,
+    code: number
+}
