0a9e02acc4c6256c8daa11df1f3af7443a9e1545
fatekkl <mathtml.1105@gmail.com>
2025-02-16 16:51:13 -0300

    add general functions
    create lastOpened interactors + updateOpenings

---
+++ src/utils/updateLastOpened.ts
@@ -0,0 +1,24 @@
+import { Env } from "../../worker-configuration";
+import { ApiResponse } from "../models/types";
+
+async function updateLastOpened(email: string, date: string, env: Env): Promise<ApiResponse> {
+  const query = "UPDATE users SET last_open_date = ? WHERE email = ?";
+  
+  try {
+    const result = await env.D1_DB.prepare(query).bind(date, email).run();
+
+    if (!result.success) {
+      throw new Error("A atualização não foi realizada com sucesso.");
+    }
+
+    return { 
+      success: true, 
+      data: `${date} é a última data aberta do usuário ${email}`, 
+      code: 200 
+    };
+  } catch (error: any) {
+    throw new Error(`Erro ao registrar atualizar a última data do usuário ${email}: ${error.message}`);
+  }
+}
+
+export default updateLastOpened;
---
+++ src/utils/updateOpenings.ts
@@ -0,0 +1,28 @@
+import { ApiResponse } from "../models/types";
+import { Env } from "../../worker-configuration";
+
+async function updateOpenings(email: string, env: Env): Promise<ApiResponse> {
+  const query = "UPDATE users SET openings = openings + 1 WHERE email = ?;";
+  
+  try {
+    const result = await env.D1_DB.prepare(query).bind(email).run();
+    
+    if (!result.success) {
+      throw new Error("Erro ao tentar atualizar a base de dados");
+    }
+    
+    return {
+      success: true,
+      data: { message: `As openings de ${email} foram incrementadas em +1` },
+      code: 200,
+    };
+  } catch (error: any) {
+    throw {
+      success: false,
+      data: { message: `Erro ao tentar atualizar a base de dados: ${error.message}` },
+      code: 500,
+    };
+  }
+}
+
+export default updateOpenings;
